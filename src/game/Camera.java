834a6ca9dbe3e5b01a968ac1a8235f547c4f2225
HP <HP@Alex.lan>
2017-01-11 01:37:09 -0600

    Drunk edits yo

--- src/game/Camera.java
+++ src/game/Camera.java
@@ -1,26 +1,118 @@
 package game;
 
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
+
 import sub.Point3D;
 import sub.Vector3D;
 
 public class Camera {
 	Point3D camLoc;
 	Vector3D camOrient;
-	boolean up = false;
-	boolean down = false;
+	Set<Boolean> keysPressed = new HashSet<Boolean>();
+	boolean shift = false;
 	boolean forward = false;
 	boolean backward = false;
 	boolean left = false;
 	boolean right = false;
-	boolean test;
+	boolean up = false;
+	boolean down = false;
 	int speed = 1;
 
 	public Camera(Point3D loc, Vector3D orient){
 		this.camLoc = loc;
 		this.camOrient = orient;
 	}
-	
+
 	public void tick(){
+		// Check DIRECTION and opposite axial DIRECTION
+		//   If both, do nothing
+		//   Else If one, check for shift
+		//     If shift, check:
+		//       If 
+		//     If shift, rotate in that dual axis plane
+		//     Else move in that direction
+		//   Else check next direction
+
+		// XY: Yaw   ( left / right )
+		// XZ: Roll  ( shift + left / right )
+		// YZ: Pitch ( shift + forward / backward )
+
+		if(shift){
+			if(forward){
+				if(backward){
+					// Do nothing
+				}else{
+					// Shift + forward
+					// = Pitch down
+					// = Rotate on YZ CW : Negative
+				}
+			}else if(backward){
+				// Shift + backward
+				// = Pitch up
+				// = Rotate on YZ CCW : Positive
+			}
+			if(left){
+				if(right){
+					// Do nothing
+				}else{
+					// Shift + left
+					// = Roll left
+					// = Rotate on XZ CCW : Positive
+				}
+			}else if(right){
+				// Shift + right
+				// = Roll right
+				// = Rotate on XZ CW : Negative
+			}
+		}else{
+			// No shift
+			if(forward){
+				if(backward){
+					// Do nothing
+				}else{
+					// Forward
+					// = Move forward
+					// Add to x, y, z speed * (normalized)[x, y, z]
+				}
+			}else if(backward){
+				// Backward
+				// = Move backward
+				// Add to x, y, z -1 * speed * (normalized)[x, y, z]
+			}
+
+			if(left){
+				if(right){
+					// Do nothing
+				}else{
+					// Left
+					// = Left
+					// = Rotate on XY CCW : Positive
+				}
+			}else if(right){
+				// Right
+				// = Right
+				// = Rotate on XY CW : Negative
+			}
+			if(up){
+				if(down){
+					return;
+				}else{
+					// Up
+					// = Up
+					// = Move upward
+				}
+			}else if(down){
+				// Down
+				// = Pitch down
+				// = Rotate on YZ CW : Negative
+			}
+		}
+
+		if(shift){
+			System.out.println("Shift!!\n\n\n\n\nShift!!");
+		}
 		if(forward){
 			//System.out.println("Forward");
 			camLoc.y += speed;
@@ -128,13 +220,19 @@ public class Camera {
 		this.right = right;
 	}
 
-	/** @category getter */
-	public boolean isTest() {
-		return test;
+	public boolean isShift() {
+		return shift;
 	}
 
-	/** @category setter */
-	public void setTest(boolean test) {
-		this.test = test;
+	public void setShift(boolean shift) {
+		this.shift = shift;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
 	}
 }
--- src/game/KeyInput.java
+++ src/game/KeyInput.java
@@ -13,14 +13,50 @@ public class KeyInput extends KeyAdapter {
 
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		
-		//System.out.println("Here\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHere");
-		if(key == KeyEvent.VK_W) handler.camera.setForward(true);
-		if(key == KeyEvent.VK_S) handler.camera.setBackward(true);
-		if(key == KeyEvent.VK_D) handler.camera.setRight(true);
-		if(key == KeyEvent.VK_A) handler.camera.setLeft(true);
-		if(key == KeyEvent.VK_SPACE) handler.camera.setUp(true);
-		if(key == KeyEvent.VK_CONTROL) handler.camera.setDown(true);
+
+		switch(key){
+		case KeyEvent.VK_W:
+			// If also shift, then roll forward = true
+			break;
+		case KeyEvent.VK_A:
+			break;
+		case KeyEvent.VK_S:
+			break;
+		case KeyEvent.VK_D:
+			break;
+		case KeyEvent.VK_CONTROL:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		case KeyEvent.VK_SHIFT:
+			break;
+		default:
+			break;
+		}
+
+		/*
+		if(key == KeyEvent.VK_W){
+			handler.camera.setForward(true);
+		}
+		if(key == KeyEvent.VK_S){ handler.camera.setBackward(true);
+
+		}
+		if(key == KeyEvent.VK_D){
+			handler.camera.setRight(true);
+		}
+		if(key == KeyEvent.VK_A){
+			handler.camera.setLeft(true);
+		}
+		if(key == KeyEvent.VK_SPACE){
+			handler.camera.setUp(true);
+		}
+		if(key == KeyEvent.VK_CONTROL){
+			handler.camera.setDown(true);
+		}
+		if(key == KeyEvent.VK_SHIFT){
+			handler.camera.setShift(true);
+		}
+		*/
 	}
 
 	public void keyReleased(KeyEvent e) {
@@ -33,6 +69,7 @@ public class KeyInput extends KeyAdapter {
 		if(key == KeyEvent.VK_A) handler.camera.setLeft(false);
 		if(key == KeyEvent.VK_SPACE) handler.camera.setUp(false);
 		if(key == KeyEvent.VK_CONTROL) handler.camera.setDown(false);
+		if(key == KeyEvent.VK_SHIFT) handler.camera.setShift(false);
 	}
 
 }
