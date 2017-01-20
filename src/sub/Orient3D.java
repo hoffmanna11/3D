b553ce54f8d6bb92e038802ad1d3e918a211dbfe
thisguy <hoffmanna111@gmail.com>
2017-01-20 04:08:17 -0600

    Restoring Orient3D

---
+++ src/sub/Orient3D.java
@@ -0,0 +1,13 @@
+package sub;
+
+public class Orient3D {
+	public Vector3D xy;
+	public Vector3D yz;
+	public Vector3D xz;
+	
+	public Orient3D(Vector3D xy, Vector3D yz, Vector3D xz){
+		this.xy = xy;
+		this.yz = yz;
+		this.xz = xz;//sskkkk
+	}
+}
