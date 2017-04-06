0dc5fecc141b680883acc1f34a9cbf6e083c5978
Admin <hoffmanna111@gmail.com>
2017-04-06 07:16:14 -0500

    asdf

--- src/overlays/Grid.java
+++ src/overlays/Grid.java
@@ -29,7 +29,7 @@ public class Grid extends Overlay {
 	}
 
 	public void render(Graphics g) {
-		this.g = g;
+		Grid.g = g;
 		drawGrid();
 	}
 
---
+++ src/sub/Vector.java
@@ -0,0 +1,15 @@
+package sub;
+
+public class Vector {
+	double[][] values;
+	
+	Vector(double[][] values){
+		
+	}
+	
+	boolean validVector(double[][] values){
+		// Evaluate all invalid vector cases
+		
+		return true;
+	}
+}
