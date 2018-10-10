1e33a0528eca9fa0955abe73a180bdade0ad9fc0
me <hoffmanna111@gmail.com>
2018-10-10 01:33:58 -0500

    kjh

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,6 +2,13 @@ package game;
 
 public class Main {
 	public static void main(String args[]) {
-		Env level1 = new Env();
+		Env gameEnv = new Env();
+		// gameEnv.load(level1);
+		//   loads all the characters, scenery into memory
+		//   starts their tick cycles
+		// gameEnv.addPlayer(mainboy);
+		// gameEnv.start();
+		//   moves camera to level location, pinned to focused character
+		// 
 	}
 }
