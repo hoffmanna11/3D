439d402e050ec246a1c04234defce1b6052a8afc
me <hoffmanna111@gmail.com>
2018-11-09 21:28:24 -0600

    Condensing controls overlay

--- src/overlays/Controls.java
+++ src/overlays/Controls.java
@@ -6,16 +6,14 @@ import object_categories.Overlay;
 
 public class Controls extends Overlay {
 	public Controls() {
-		addOffset(); // Movement
+		addOffset(); // Movement and Rotation
 		addOffset(); // Speed
-		addOffset(); // Rotation
 	}
 	
 	@Override
 	public void render(Graphics g) {
-		setMessage(0, "E: Forward, D: Backward, S: Left, F: Right");
+		setMessage(0, "E: Forward, D: Backward, S: Left, F: Right, A, G to rotate");
 		setMessage(1, "Hold left click to speed up, middle click to slow down");
-		setMessage(2, "A, G to rotate");
 		renderStrings(g);
 	}
 	
