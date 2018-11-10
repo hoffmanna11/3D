2f3e6dbc5b65c56c29db758cfdecb044b6df3de2
me <hoffmanna111@gmail.com>
2018-11-09 21:30:25 -0600

    Test

--- src/overlays/Controls.java
+++ src/overlays/Controls.java
@@ -12,7 +12,7 @@ public class Controls extends Overlay {
 	
 	@Override
 	public void render(Graphics g) {
-		setMessage(0, "E: Forward, D: Backward, S: Left, F: Right, A, G to rotate");
+		setMessage(0, "E: Forward, D: Backward, S: Left, F: Right, A, G to rotate ");
 		setMessage(1, "Hold left click to speed up, middle click to slow down");
 		renderStrings(g);
 	}
